30934c2389ba39103530b461665576a2770f09fb
wujinzhe <wujinzhe@wujinzhedeiMac.local>
2016-11-22 21:53:16 -0800

    init ejs engin

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    tt:'这个是一个变量'
+  });
 });
 
 module.exports = router;
