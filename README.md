88ac2b197a7d5e134445ef903f087e98e033421b
wujinzhe <wujinzhe@wujinzhedeiMac.local>
2016-11-28 18:17:36 +0800

    modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wroadw
 个人网站
+1.node.js 使用nodemon 实时监控文件的修改 npm install -g nodemon 全局安装
+2.cd到网站目录 nodemon 开启站点 
--- app.js
+++ app.js
@@ -20,9 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));   //该文件中的文件为静态文件，可以直接拿访问
 
 app.use('/', index);
+//
 app.use('/users', users);
 
 // catch 404 and forward to error handler
